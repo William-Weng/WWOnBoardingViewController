2f4004ba37ef9a2381c6e0b032f9299acdab0360
William.Weng <William.Weng@iit.com.hk>
2023-12-29 11:48:07 +0800

    - 修正範例

--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -22,6 +22,7 @@ final class ViewController: UIViewController {
         ]
     }()
     
+    private let currentPage = 0
     private var onBoardingViewController: WWOnBoardingViewController?
 
     override func viewDidLoad() {
@@ -43,7 +44,7 @@ final class ViewController: UIViewController {
 
 // MARK: - WWOnBoardingViewControllerDelegate
 extension ViewController: WWOnBoardingViewControllerDelegate {
-   
+    
     func viewControllers(onBoardingViewController: WWOnBoardingViewController) -> [UIViewController] {
         return pageViewControllerArray
     }
@@ -61,8 +62,9 @@ private extension ViewController {
     ///   - segue: UIStoryboardSegue
     ///   - sender: Any?
     func initSetting(for segue: UIStoryboardSegue, sender: Any?) {
+        
         onBoardingViewController = segue.destination as? WWOnBoardingViewController
-        onBoardingViewController?.setting(onBoardingDelegate: self, isInfinityLoop: true, currentIndex: 1)
+        onBoardingViewController?.setting(onBoardingDelegate: self, isInfinityLoop: true, currentIndex: currentPage)
     }
     
     /// 尋找Storyboard上的ViewController for StoryboardId
@@ -75,11 +77,13 @@ private extension ViewController {
     /// [PageControl設定](https://medium.com/彼得潘的-swift-ios-app-開發問題解答集/ios-14-進化的-page-control-f097af2801a6)
     func pageContolSetting() {
 
+        pageControl.allowsContinuousInteraction = true
         pageControl.numberOfPages = pageViewControllerArray.count
+        pageControl.currentPage = currentPage
         pageControl.backgroundStyle = .prominent
+        
         // pageControl.preferredIndicatorImage = UIImage(systemName: "sun.max.fill")
         (0..<pageControl.numberOfPages).forEach { pageControl.setIndicatorImage(UIImage(systemName: "\($0).circle"), forPage: $0) }
-        
         pageControl.addTarget(self, action: #selector(changeCurrentPage(_:)), for: .valueChanged)
     }
 }
